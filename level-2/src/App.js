515ac94fdb403174cf425745ca97963c09ebcd3e
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2025-10-29 00:21:42 +0300

    Fix import statements in App.js

--- level-2/src/App.js
+++ level-2/src/App.js
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-
 function App() {
   return (
     <Router>
